b6ba4721b04bf716ab9b4df07cf6b402c60a6043
Alex Souza <alexjoseps@outlook.com>
2022-11-16 23:58:43 -0300

    feat: router with routes for ours use cases

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+import { router } from './router';
+
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017')
@@ -8,6 +10,8 @@ mongoose.connect('mongodb://localhost:27017')
     console.log('Connected to MongoDB');
 
     const port = 3001;
+
+    app.use(router);
 
     app.listen(port, () => {
       console.log(`🚀 Server is running on http://localhost:${port}`);
