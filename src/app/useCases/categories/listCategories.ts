562f9fb405342b50fbcc7f92b2f3fec593e32ea4
Alex Souza <alexjoseps@outlook.com>
2022-11-20 23:43:57 -0300

    feat: list categories use case with try catch

--- src/app/useCases/categories/listCategories.ts
+++ src/app/useCases/categories/listCategories.ts
@@ -2,7 +2,11 @@ import { Request, Response } from 'express';
 import { Category } from '../../models/Category';
 
 export async function listCategories(req: Request, res: Response) {
-  const categories = await Category.find();
+  try {
+    const categories = await Category.find();
 
-  res.json(categories);
+    res.status(200).json(categories);
+  } catch (error) {
+    res.sendStatus(500);
+  }
 }
